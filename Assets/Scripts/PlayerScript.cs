50cbef24b9231667355622793a8d9f56536efd92
zyiology <zyiology10000@gmail.com>
2020-06-11 17:51:38 +0800

    Exercise 3

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,9 +6,16 @@ using Mirror;
 
 public class PlayerScript : NetworkBehaviour
 {
+	[SyncVar]
+	public bool isReady = false;
+	
+	[SyncVar]
+	public bool gameStart = false;
+	
 	public List<Material> materialList = new List<Material>();
 	public Button choiceButtonPrefab;
 	private Transform canvasTransform;
+	private Button readyButton;
 	
     public override void OnStartClient()
     {
@@ -16,9 +23,10 @@ public class PlayerScript : NetworkBehaviour
         if (!base.hasAuthority) {
 			return;
 		}
-		
-		
+				
 		canvasTransform = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<Transform>();
+		readyButton = GameObject.FindGameObjectWithTag("ReadyButton").GetComponent<Button>();
+		readyButton.onClick.AddListener(CmdChangeReady);
 		
 		for (int i=0; i<materialList.Count; i++) {
 			Button choiceButton = Instantiate(choiceButtonPrefab, canvasTransform);
@@ -30,6 +38,34 @@ public class PlayerScript : NetworkBehaviour
 			choiceButton.onClick.AddListener(delegate {CmdSetTexture(temp);});
 		}
     }
+	
+	[Command]
+	void CmdChangeReady() {
+		isReady ^= true;//toggles isReady
+		TargetChangeReady(isReady);
+		
+		List<PlayerScript> playerScriptList = new List<PlayerScript>();
+		foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")) {
+			PlayerScript ps = player.GetComponent<PlayerScript>();
+			if (ps.isReady == false) {
+					return;
+			}
+			playerScriptList.Add(ps);
+		}
+		
+		//only runs if all players are ready
+		foreach (PlayerScript ps in playerScriptList) {
+			ps.gameStart = true;
+		}
+	}
+	
+	[TargetRpc]
+	//no NetworkConnection specified - ie it always targets the owner of this object 
+	//why include bool toChange - there's latency when the server updates the syncvar, so include it in the function to be safe
+	void TargetChangeReady(bool change) {
+		readyButton.GetComponentInChildren<Text>().text = change ? "READY!!!" : "Not Ready...";
+	}
+		
 	
 	[Command]
 	void CmdSetTexture(int index) {
@@ -47,6 +83,11 @@ public class PlayerScript : NetworkBehaviour
         if (!base.hasAuthority) {
 			return;
 		}
+		
+		if (!gameStart) {
+			return;
+		}
+		
 		float xMov = Input.GetAxis("Horizontal");
 		float zMov = Input.GetAxis("Vertical");
 		CmdMoveCube(xMov, zMov);
